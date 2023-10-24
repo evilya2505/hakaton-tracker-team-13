1874766ec0e5925dc3bdfb08d089654ed40c4330
Anna <thoria@yandex.ru>
2023-10-24 17:03:28 +0300

    add styles to nav links

--- src/components/main/main-nav-links/index.tsx
+++ src/components/main/main-nav-links/index.tsx
@@ -5,16 +5,24 @@ const MainNavLinks: React.FC<{}> = (): JSX.Element => {
   return (
     <ul className={mainNavLinks.list}>
       <li>
-        <Link to="/applicants" className={mainNavLinks.link}>Соискатели</Link>
+        <Link to="/applicants" className={`${mainNavLinks.link} ${mainNavLinks.link_type_applicants}`}>
+          Соискатели
+        </Link>
       </li>
       <li>
-        <Link to="/vacancies" className={mainNavLinks.link}>Мои вакансии</Link>
+        <Link to="/vacancies" className={`${mainNavLinks.link} ${mainNavLinks.link_type_vacancies}`}>
+          Мои вакансии
+        </Link>
       </li>
       <li>
-        <Link to="/statistics" className={mainNavLinks.link}>Статистика</Link>
+        <Link to="/statistics"className={`${mainNavLinks.link} ${mainNavLinks.link_type_statistics}`}>
+          Статистика
+        </Link>
       </li>
       <li>
-        <Link to="/planer" className={mainNavLinks.link}>Планер</Link>
+        <Link to="/planer" className={`${mainNavLinks.link} ${mainNavLinks.link_type_planer}`}>
+          Планер
+        </Link>
       </li>
     </ul>
   );
