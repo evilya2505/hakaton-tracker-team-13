4ccca129774ed38953088c29719a0756cf1e7b12
Anna <thoria@yandex.ru>
2023-10-25 03:03:30 +0300

    refactor NavLinks

--- src/components/main/main-nav-links/index.tsx
+++ src/components/main/main-nav-links/index.tsx
@@ -1,39 +1,21 @@
 import mainNavLinks from "./index.module.css";
-import { Link } from "react-router-dom";
+import MainNavLink from "../main-nav-link";
+import { navLinksList } from "../../../constants/navLinksList";
 
 const MainNavLinks: React.FC<{}> = (): JSX.Element => {
   return (
     <ul className={mainNavLinks.list}>
-      <li>
-        <Link to="/applicants" className={`${mainNavLinks.link} ${mainNavLinks.link_type_applicants}`}>
-          Соискатели
-        </Link>
-      </li>
-      <li>
-        <Link to="/vacancies" className={`${mainNavLinks.link} ${mainNavLinks.link_type_vacancies}`}>
-          Мои вакансии
-        </Link>
-      </li>
-      <li>
-        <Link to="/statistics"className={`${mainNavLinks.link} ${mainNavLinks.link_type_statistics}`}>
-          Статистика
-        </Link>
-      </li>
-      <li>
-        <Link to="/planer" className={`${mainNavLinks.link} ${mainNavLinks.link_type_planer}`}>
-          Планер
-        </Link>
-      </li>
-      <li className={mainNavLinks.item}>
-        <Link to="/settings" className={`${mainNavLinks.link} ${mainNavLinks.link_type_settings}`}>
-          Настройки
-        </Link>
-      </li>
-      <li>
-        <Link to="/" className={`${mainNavLinks.link} ${mainNavLinks.link_type_logout}`}>
-          Выход
-        </Link>
-      </li>
+      {navLinksList.map((element: { name: string; title: string }, index) => {
+        return (
+          <li className={mainNavLinks.item}>
+            <MainNavLink
+              key={index}
+              name={element.name}
+              title={element.title}
+            />
+          </li>
+        );
+      })}
     </ul>
   );
 };
