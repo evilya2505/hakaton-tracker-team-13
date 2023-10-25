9d9fe704c543315776649d6d71137f0bc864d9f6
Vova iz Tambova <voller.ruden@yandex.ru>
2023-10-25 15:31:56 +0300

    add vacancy minicard

---
+++ src/components/main/vacancies/vacancies-card/index.tsx
@@ -0,0 +1,24 @@
+import vacanciesCard from "./index.module.css";
+// import DeleteIcon from '@mui/icons-material/Delete';
+
+const VacanciesCard = () => {
+  return (
+    <section className={vacanciesCard.card}>
+      <div className={vacanciesCard.vacancy}>
+        <h4 className={vacanciesCard.name}>UX/UI-дизайнер (Junior)</h4>
+        <div className={vacanciesCard.settings}>
+          <div className={vacanciesCard.edit} onClick={() => { }} />
+          <div className={vacanciesCard.delete} onClick={() => { }} />
+        </div>
+      </div>
+      <p className={vacanciesCard.status}>Подходящие кандидаты: 54</p>
+      <p className={vacanciesCard.status}>Откликнулось: 0</p>
+      <div className={vacanciesCard.dataInfo}>
+        <div className={vacanciesCard.dataTitle}>Дата публикации</div>
+        <div className={vacanciesCard.dataCreate}>20 сентября 2023</div>
+      </div>
+    </section>
+  );
+}
+export default VacanciesCard;
+
