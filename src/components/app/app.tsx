4ad3dc10a848f51b27a34808773ded256b4bc7d3
Эвелина Ким <elya@MacBook-Pro-Evelina.local>
2023-10-26 18:12:08 +0300

    fix: удалить ненужные компоненты из App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -7,19 +7,8 @@ import Applicants from "../main/applicants";
 import Vacancies from "../main/vacancies";
 import Statistics from "../main/statistics";
 import Planer from "../main/planer";
-import BasicModal from "../modal/modal";
-import VacanciesModal from "../vacancies-modal/vacancies-modal";
-import { Button } from "@mui/material";
+
 function App() {
-  const [isAddVacancyModalOpened, setIsAddVacancyModalOpened] = React.useState<boolean>(false);
-
-  function handleOpenVacancyModal() {
-    setIsAddVacancyModalOpened(true);
-  }
-
-  function handleCloseModal() {
-    setIsAddVacancyModalOpened(false);
-  }
   return (
     <>
       <Routes>
@@ -39,10 +28,6 @@ function App() {
         </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
-      <Button onClick={handleOpenVacancyModal}>test</Button>
-      <BasicModal isVisible={isAddVacancyModalOpened} closePopup={handleCloseModal}>
-        <VacanciesModal />
-      </BasicModal>
     </>
   );
 }
