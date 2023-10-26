aff35148d128f94d0b739b77d0b8de71d9452381
Anna <thoria@yandex.ru>
2023-10-27 02:15:45 +0300

    add interactive activityScale

--- src/components/main/applicants/applicants-card/activity-scale/index.tsx
+++ src/components/main/applicants/applicants-card/activity-scale/index.tsx
@@ -1,4 +1,6 @@
 import activity from "./index.module.css";
+import renderScale from "../../../../../utils/renderActivityScale";
+import CustomTooltip from "../tooltip";
 
 interface ActivityScaleProps {
   responses: number;
@@ -11,11 +13,39 @@ const ActivityScale = ({
   completedTestTasks,
   interviews,
 }: ActivityScaleProps): JSX.Element => {
+  let res = renderScale(responses, completedTestTasks, interviews);
+
   return (
     <div className={activity.scale}>
-      <div className={activity.responses}>{responses}</div>
-      <div className={activity.testing}>{completedTestTasks}</div>
-      <div className={activity.interview}>{interviews}</div>
+      <CustomTooltip title="Отклики" placement="top" color="secondary" arrow>
+        <div
+          className={activity.responses}
+          style={{ width: `${res.responses}%` }}
+        >
+          {!isNaN(res.responses) && res.responses !== 0 ? responses : ""}
+        </div>
+      </CustomTooltip>
+      <CustomTooltip title="Тестовые" placement="top" color="secondary" arrow>
+        <div
+          className={activity.testing}
+          style={{ width: `${res.completedTestTasks}%` }}
+        >
+          {!isNaN(res.completedTestTasks) && res.completedTestTasks !== 0 ? completedTestTasks : ""}
+        </div>
+      </CustomTooltip>
+      <CustomTooltip
+        title="Собеседования"
+        placement="top"
+        color="secondary"
+        arrow
+      >
+        <div
+          className={activity.interview}
+          style={{ width: `${res.interviews}%` }}
+        >
+          {!isNaN(res.interviews) && res.interviews !== 0 ? interviews : ""}
+        </div>
+      </CustomTooltip>
     </div>
   );
 };
