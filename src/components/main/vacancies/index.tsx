ab09543b6d52fb90bcf70eea42ffc568dc8d5b5b
Anna <thoria@yandex.ru>
2023-10-26 04:10:38 +0300

    refactor vacancies

--- src/components/main/vacancies/index.tsx
+++ src/components/main/vacancies/index.tsx
@@ -1,41 +1,13 @@
 import { useState } from "react";
 import vacancies from "./index.module.css";
 import page from "../index.module.css";
-import { Tabs, Tab, Box } from "@mui/material";
+import { Tab } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 import VacanciesCard from "./vacancies-card";
 import BasicModal from "../../modal/modal";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function CustomTabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
 const Vacancies: React.FC<{}> = (): JSX.Element => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState("1");
   const [isBellModalVisible, setIsBellModalVisible] = useState<boolean>(false);
 
   function openModal() {
@@ -46,7 +18,7 @@ const Vacancies: React.FC<{}> = (): JSX.Element => {
     setIsBellModalVisible(false);
   }
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
   return (
@@ -59,33 +31,30 @@ const Vacancies: React.FC<{}> = (): JSX.Element => {
           onClick={openModal}
         ></button>
       </div>
-      <Box>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="Активные" {...a11yProps(0)} />
-          <Tab label="Черновики" {...a11yProps(1)} />
-          <Tab label="Архив" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={value} index={0}>
-        <VacanciesCard />
-        {/* <div className={vacancies.activeVacancies}>
-          <p>Вы еще не создали ни одной вакансии</p>
-        </div> */}
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        <div className={vacancies.activeVacancies}>
-          <p>Вы еще не создали ни одного черновика</p>
-        </div>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        <div className={vacancies.activeVacancies}>
-          <p>Нет ни одной вакансии в архиве</p>
-        </div>
-      </CustomTabPanel>
+      <TabContext value={value}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="Активные" value="1" />
+          <Tab label="Черновики" value="2" />
+          <Tab label="Архив" value="3" />
+        </TabList>
+        <TabPanel value="1">
+          <VacanciesCard />
+          {/* <div className={vacancies.activeVacancies}>
+            <p>Вы еще не создали ни одной вакансии</p>
+          </div> */}
+        </TabPanel>
+        <TabPanel value="2">
+          <div className={vacancies.activeVacancies}>
+            <p>Вы еще не создали ни одного черновика</p>
+          </div>
+        </TabPanel>
+        <TabPanel value="3">
+          <div className={vacancies.activeVacancies}>
+            <p>Нет ни одной вакансии в архиве</p>
+          </div>
+        </TabPanel>
+      </TabContext>
+
       <BasicModal closePopup={closeModal} isVisible={isBellModalVisible}>
         <div>Новые уведомления</div>
       </BasicModal>
