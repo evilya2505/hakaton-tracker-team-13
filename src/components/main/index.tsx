3e07cf70406d1a4ef5fa9a82eb1006b2ccfd20ac
Anna <thoria@yandex.ru>
2023-10-27 03:38:40 +0300

    add navigate to applicants at first main render

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,8 +1,13 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import main from "./index.module.css";
 import Navigation from "./common/main-navigation";
+import { useNavigate } from "react-router";
 
 const Main: React.FC<PropsWithChildren> = ({ children }): JSX.Element => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/applicants");
+  }, []);
   return (
     <main className={main.page}>
       <Navigation />
