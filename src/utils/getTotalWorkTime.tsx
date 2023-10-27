bf6c5bd1ef91c732e30047a3ef49e3eeeca24233
Anna <thoria@yandex.ru>
2023-10-27 20:12:49 +0300

    add margin to appl list

---
+++ src/utils/getTotalWorkTime.tsx
@@ -0,0 +1,5 @@
+export const getTotalWorkTime = (from: Date, to: Date) => {
+  //     const diffTime = Math.abs(to.toLocaleString() - from.toLocaleString());
+  // return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return to;
+};
