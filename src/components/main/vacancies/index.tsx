2ddf91b75a36995980a3a86a2a1b1d82251aa1d6
Evelina Kim <like17seventeen@gmail.com>
2023-10-28 20:36:57 +0300

    fix: поправить ошибка после конфликта

--- src/components/main/vacancies/index.tsx
+++ src/components/main/vacancies/index.tsx
@@ -6,7 +6,7 @@ import { TabContext, TabList, TabPanel } from "@mui/lab";
 import VacanciesCard from "./vacancies-card";
 import BasicModal from "../../modal/modal";
 // import NotifyBell from "./notify-button";
-import BasicPopover from "./notify-modal"
+import BasicPopover from "./notify-modal";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import CreateVacancyButton from "./create-button";
@@ -39,10 +39,6 @@ const Vacancies: React.FC<{}> = (): JSX.Element => {
   //   setIsBellModalVisible(false);
   // }
 
-  function handleCardClick() {
-    console.log('open vacancy');
-  }
-
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
@@ -53,64 +49,69 @@ const Vacancies: React.FC<{}> = (): JSX.Element => {
 
   return (
     <div className={`${page.pageElement} ${vacanciesPage.container}`}>
-      <div className={vacanciesPage.titleContainer}>
-        <h2 className={vacanciesPage.title}>Мои вакансии</h2>
-        <BasicPopover />
-      </div>
-      <TabContext value={value}>
-        <TabList onChange={handleChange} aria-label="lab API tabs example">
-          <Tab label="Активные" value="1" />
-          <Tab label="Черновики" value="2" />
-          <Tab label="Архив" value="3" />
-        </TabList>
-        <TabPanel className={vacanciesPage.list} value="1">
-          <ul className={vacanciesPage.list}>
-            {vacancies.map((element: vacancy, index) => {
-              return (
-                <li key={index} onClick={handleCardClick}>
-                  <VacanciesCard vacancy={element} />
-                </li>
-              );
-            })}
-          </ul>
-          {/* <div className={vacancies.activeVacancies}>
+      {location.pathname.includes("vacancy") ? (
+        <Outlet />
+      ) : (
+        <>
+          <div className={vacanciesPage.titleContainer}>
+            <h2 className={vacanciesPage.title}>Мои вакансии</h2>
+            <BasicPopover />
+          </div>
+          <TabContext value={value}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Активные" value="1" />
+              <Tab label="Черновики" value="2" />
+              <Tab label="Архив" value="3" />
+            </TabList>
+            <TabPanel className={vacanciesPage.list} value="1">
+              <ul className={vacanciesPage.list}>
+                {vacancies.map((element: vacancy, index) => {
+                  return (
+                    <li key={index} onClick={handleCardClick}>
+                      <VacanciesCard vacancy={element} />
+                    </li>
+                  );
+                })}
+              </ul>
+              {/* <div className={vacancies.activeVacancies}>
             <p>Вы еще не создали ни одной вакансии</p>
           </div> */}
-          <div className={vacanciesPage.buttonFixed}>
-            <Fab
-              color="primary"
-              aria-label="add"
-              onClick={handleOpenVacancyModal}
-            >
-              <AddIcon />
-            </Fab>
-          </div>
-        </TabPanel>
-        <TabPanel value="2">
-          <div className={vacanciesPage.activeVacancies}>
-            <p>Вы еще не создали ни одного черновика</p>
+              <div className={vacanciesPage.buttonFixed}>
+                <Fab
+                  color="primary"
+                  aria-label="add"
+                  onClick={handleOpenVacancyModal}
+                >
+                  <AddIcon />
+                </Fab>
+              </div>
+            </TabPanel>
+            <TabPanel value="2">
+              <div className={vacanciesPage.activeVacancies}>
+                <p>Вы еще не создали ни одного черновика</p>
 
-            <CreateVacancyButton />
-
-          </div>
-        </TabPanel>
-        <TabPanel value="3">
-          <div className={vacanciesPage.activeVacancies}>
-            <p>Нет ни одной вакансии в архиве</p>
-          </div>
-        </TabPanel>
-      </TabContext>
-      {/* <BasicPopover closePopup={closeNotifyModal} isVisible={isBellModalVisible}>
+                <CreateVacancyButton />
+              </div>
+            </TabPanel>
+            <TabPanel value="3">
+              <div className={vacanciesPage.activeVacancies}>
+                <p>Нет ни одной вакансии в архиве</p>
+              </div>
+            </TabPanel>
+          </TabContext>
+          {/* <BasicPopover closePopup={closeNotifyModal} isVisible={isBellModalVisible}>
         <div>Новые уведомления</div>
       </BasicPopover> */}
 
-      <BasicModal
-        isVisible={isAddVacancyModalOpened}
-        closePopup={handleCloseVacancyModal}
-      >
-        <VacanciesModal />
-      </BasicModal>
-    </div >
+          <BasicModal
+            isVisible={isAddVacancyModalOpened}
+            closePopup={handleCloseVacancyModal}
+          >
+            <VacanciesModal />
+          </BasicModal>
+        </>
+      )}
+    </div>
   );
 };
 
